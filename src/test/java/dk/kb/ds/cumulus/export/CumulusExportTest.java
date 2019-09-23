38dfca32cbb23c3cbdf808c8fbed60c2415e79ed
Toke Eskildsen <toes@kb.dk>
2019-09-23 10:23:51 +0200

    Add basic JUnit 4.12 support and a trivial unit-test

---
+++ src/test/java/dk/kb/ds/cumulus/export/CumulusExportTest.java
@@ -0,0 +1,13 @@
+package dk.kb.ds.cumulus.export;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CumulusExportTest {
+
+    @Test
+    public void testSetup() {
+        assertTrue("Basic test run with always-pass should work", true);
+    }
+}
