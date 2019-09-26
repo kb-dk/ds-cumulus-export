b0fa09a8358dae47933ab9380d9d0032a5d465c2
Toke Eskildsen <toes@kb.dk>
2019-09-26 14:53:23 +0200

    Add YAML-based configuration

---
+++ src/test/java/dk/kb/ds/cumulus/export/ConfigurationTest.java
@@ -0,0 +1,20 @@
+package dk.kb.ds.cumulus.export;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConfigurationTest {
+
+    @Test
+    public void testConfigurationLoad() {
+        // Pretty simple: If it loads without throwing an exception, all is fine
+        Configuration.instance();
+    }
+
+    @Test
+    public void testCumulusConfigurationLoad() {
+        assertNotNull(Configuration.getCumulusConf(), "The cumulus configuration should exist");
+    }
+
+}
