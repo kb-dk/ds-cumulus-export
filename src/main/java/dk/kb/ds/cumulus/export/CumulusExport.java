cf5dfd867c57b50434698f7b51dcbefe16994db5
Dan H Egebo <dahe@kb.dk>
2019-09-27 12:08:11 +0200

    Initial implementation

--- src/main/java/dk/kb/ds/cumulus/export/CumulusExport.java
+++ src/main/java/dk/kb/ds/cumulus/export/CumulusExport.java
@@ -1,9 +1,29 @@
 package dk.kb.ds.cumulus.export;
 
+import dk.kb.cumulus.CumulusQuery;
+import dk.kb.cumulus.CumulusRecord;
+import dk.kb.cumulus.CumulusRecordCollection;
+import dk.kb.cumulus.CumulusServer;
+
+import java.io.File;
+import java.io.IOException;
+
 public class CumulusExport {
-    public static void main(String[] args) {
-        System.out.println("Cumulus exporter");
-        
-        // https://github.com/Det-Kongelige-Bibliotek/KB-Cumulus-API
+    public static void main(String[] args) throws IOException {
+//        System.out.println("Cumulus exporter");
+
+        boolean writeAccess = false;
+        try (CumulusServer server = new CumulusServer(Configuration.getCumulusConf())) {
+            String myCatalog = String.valueOf(Configuration.getCumulusConf().getCatalogs());
+            CumulusQuery query = CumulusQuery.getQueryForAllInCatalog(myCatalog);
+            CumulusRecordCollection recordCollection = server.getItems(myCatalog, query);
+
+            for (CumulusRecord record : recordCollection) {
+                String name = record.getFieldValue("Record Name");
+                File f = record.getFile();
+                record.setStringValueInField("status", "We found record named '" + name +
+                    "' with file at location: " + f.getAbsolutePath());
+            }
+        }
     }
 }
