a8dde5a0e19389497372cd181a341dbf2ecd948e
Dan H Egebo <dahe@kb.dk>
2019-10-04 09:02:23 +0200

    Add unit test for method getUTCTime

--- src/test/java/dk/kb/ds/cumulus/export/CumulusExportTest.java
+++ src/test/java/dk/kb/ds/cumulus/export/CumulusExportTest.java
@@ -10,4 +10,13 @@ public class CumulusExportTest {
     public void testSetup() {
         assertTrue(true, "Basic test run with always-pass should work");
     }
+
+    @Test
+    public void testGetUTCTime() {
+        //  Standard time: Zulu time is 1 hour ahead
+        assertEquals("2017-11-09T14:47:04Z", CumulusExport.getUTCTime("Thu Nov 09 15:47:04 CET 2017"));
+
+        // Daylight saving time: Zulu time is 2 hours ahead
+        assertEquals("2019-10-04T08:05:10Z", CumulusExport.getUTCTime("Fri Oct 04 10:05:10 CET 2019"));
+    }
 }
