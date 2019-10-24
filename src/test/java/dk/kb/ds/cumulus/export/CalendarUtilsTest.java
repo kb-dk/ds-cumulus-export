a1e555aeba0e0444ae645890da1909f4a5d6c3e5
Dan H Egebo <dahe@kb.dk>
2019-10-24 10:31:39 +0200

    Add CalenderUtils test class, work in progress

---
+++ src/test/java/dk/kb/ds/cumulus/export/CalendarUtilsTest.java
@@ -0,0 +1,20 @@
+package dk.kb.ds.cumulus.export;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalendarUtilsTest {
+
+    @Test
+    void getXmlGregorianCalendar() {
+    }
+
+    @Test
+    void getCurrentDate() {
+    }
+
+    @Test
+    void getDateTime() {
+    }
+}
