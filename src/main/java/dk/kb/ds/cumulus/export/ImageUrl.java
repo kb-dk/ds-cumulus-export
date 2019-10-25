6cfb9f8b0e3e2ce54b4bc16ea3469356050b68d6
Nader <nkh@kb.dk>
2019-10-25 11:25:23 +0200

    Modified image url from Cumulus

--- src/main/java/dk/kb/ds/cumulus/export/ImageUrl.java
+++ src/main/java/dk/kb/ds/cumulus/export/ImageUrl.java
@@ -6,7 +6,8 @@ public class ImageUrl {
         // cumulus-core-test-01:/Depot/DAM/test/Samlingsbilleder/0000/375/526/KE030219.tif
         String path1[] = url.split(".tif");
         String path2[] = path1[0].split(":/");
-        url = "https://kb-images.kb.dk/" + path2[1] + "/full/full/0/native.jpg";
+        String path = path2[1].replace("Depot/DAMX/Online_Master_Arkiv", "DAMJP2/online_master_arkiv");
+        url = "https://kb-images.kb.dk/" + path + "/full/!345,2555/0/native.jpg";
         return url;
     }
 }
