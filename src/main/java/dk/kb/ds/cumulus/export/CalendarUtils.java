0b6bfecde6303133fc60ec07c7a9dbb10f3c2bb0
Dan H Egebo <dahe@kb.dk>
2019-11-07 16:04:43 +0100

    Split getUTCTime in two: one handling single dates (getUTCTime) and one handling date ranges (getUTCTimeRange). Add parsing of month and day to be in the range 01-12 and 01-31 respectively.

--- src/main/java/dk/kb/ds/cumulus/export/CalendarUtils.java
+++ src/main/java/dk/kb/ds/cumulus/export/CalendarUtils.java
@@ -28,29 +28,21 @@ public class CalendarUtils {
 
     /** A single instance of the DatatypeFactory to prevent overlap from recreating it too often.*/
     private static DatatypeFactory factory = null;
-    private static final String FALSE = "false";
+
+    private static final String TO = " TO ";
+    private static final String DASH = "-";
 
     /**
      * ISO8601 representation with second granularity and Zulu time. Compatible with Solr datetime.
      */
-    static final DateTimeFormatter ISO8601_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+    private static final DateTimeFormatter ISO8601_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
 
     /**
      * Parse the given datetime String leniently, by iterating multiple formats until a result is derived.
      * @param datetime uncontrolled datetime representation.
-     * @return ISO-8601 representation of the given datetime padded to seconds if possible, else null.
+     * @return time range representation of the given datetime if possible, else null.
      */
-    public static String getUTCTime(String datetime) {
-        return getUTCTime(datetime, true);
-    }
-    /**
-     * Parse the given datetime String leniently, by iterating multiple formats until a result is derived.
-     * @param datetime     uncontrolled datetime representation.
-     * @param padToSeconds if true, the result will be padded to seconds, i.e. if the extracted datetime is '2019-10-29'
-     *                     then the result will be '2019-10-29T00:00:00Z'.
-     * @return ISO-8601 representation of the given datetime if possible, else null.
-     */
-    public static String getUTCTime(String datetime, boolean padToSeconds) {
+    public static String getUTCTimeRange(String datetime) {
         if (datetime == null || datetime.isEmpty()) {
             return null;
         }
@@ -61,39 +53,49 @@ public class CalendarUtils {
             (parsed = parseYearMonth(datetime)) != null ||
             (parsed = parseYearMonthDay(datetime)) != null ||
             (parsed = parseYearToYear(datetime)) != null
-            ) {
-            return ensurePadding(parsed, padToSeconds);
+        ) {
+            return parsed;
         }
         log.debug("Unable to parse datetime '{}'", datetime);
         return null;
     }
 
-    private final static Pattern RANGE_PATTERN = Pattern.compile(".* TO .*");
     /**
-     * Checks if input has " TO " i.e. is a solr date_range value.
-     * @param str  a parsed date value.
-     * @return true/false.
+     * Parse the given datetime String leniently, by iterating multiple formats until a result is derived.
+     * @param datetime     uncontrolled datetime representation.
+     *  the result will be padded to seconds, i.e. if the extracted datetime is '2019-10-29'
+     *  then the result will be '2019-10-29T00:00:00Z'.
+     * @return ISO-8601 representation of the given datetime if possible, else null.
      */
-    private static boolean isRangeFormat(String str){
-         Matcher rangeMatcher = RANGE_PATTERN.matcher(str);
-         return rangeMatcher.matches();
+    public static String getUTCTime(String datetime) {
+        if (datetime == null || datetime.isEmpty()) {
+            return null;
+        }
+
+        String parsed;
+        if ((parsed = parseFullWritten(datetime)) != null ||
+            (parsed = parseYear(datetime)) != null ||
+            (parsed = parseYearMonth(datetime)) != null ||
+            (parsed = parseYearMonthDay(datetime)) != null
+            ) {
+            return ensurePadding(parsed);
+        }
+        log.debug("Unable to parse datetime '{}'", datetime);
+        return null;
     }
+
 
     private static final String PADDING = "0000-01-01T00:00:00Z";
     /**
      * Pads a given input to second granularity and with Zulu time designation.
      * @param datetime     a partial ISO8601 datetime.
-     * @param padToSeconds if true, the input will be padded to seconds, else it will be returned unchanged.
-     * @return datetime padded as requested.
+     * @return datetime padded to seconds.
      */
-    private static String ensurePadding(String datetime, boolean padToSeconds) {
-        if (!padToSeconds || isRangeFormat(datetime)) {
-            return datetime;
-        }
+    private static String ensurePadding(String datetime) {
         return datetime + PADDING.substring(datetime.length());
     }
 
-    static final DateTimeFormatter WRITTEN_PARSER = DateTimeFormatter.ofPattern("ccc LLL dd HH:mm:ss zzz yyy");
+    private static final DateTimeFormatter WRITTEN_PARSER = DateTimeFormatter.ofPattern("ccc LLL dd HH:mm:ss zzz yyy");
     /**
      * Parses inputs with full date and time in written format.
      * @param datetime written time representation, e.g. 'Mon Jul 29 16:10:29 CEST 2019'.
@@ -125,25 +127,25 @@ public class CalendarUtils {
         return null;
     }
 
-    private final static Pattern YEAR_MONTH_PATTERN = Pattern.compile("^([0-9]{4}).([0-9]{2})$");
+    private final static Pattern YEAR_MONTH_PATTERN = Pattern.compile("^([0-9]{4}).(0[1-9]|1[012])$");
     /**
      * Parses inputs consisting of exactly 4 digits (year), a non-digit character and 2 digits (month).
-     * @param datetime year.month, e.g. '2019-10'.
+     * @param datetime year.month, e.g. '2019-10'
      * @return Truncated ISO-8601 representation of the given datetime if possible, else null.
      */
     private static String parseYearMonth(String datetime) {
         Matcher ymMatcher = YEAR_MONTH_PATTERN.matcher(datetime);
         if (ymMatcher.matches()) {
-            return ymMatcher.group(1) + "-" + ymMatcher.group(2);
+            return ymMatcher.group(1) + DASH + ymMatcher.group(2);
         }
         log.trace("parseYearMonth({}) failed parsing", datetime);
         return null;
     }
 
-    private final static Pattern YEAR_MONTH_DAY_PATTERN = Pattern.compile("^([0-9]{4}).([0-9]{2}).([0-9]{2})$");
+    private final static Pattern YEAR_MONTH_DAY_PATTERN = Pattern.compile("^([0-9]{4}).(0[1-9]|1[012]).(0[1-9]|[12][0-9]|3[01])$");
     /**
-     * Parses inputs consisting of exactly 4 digits (year), a non-digit character, 2 digits (month), a non-digit character
-     * and 2 digits (day).
+     * Parses inputs consisting of exactly 4 digits (year), a non-digit character, 2 digits (month, 01-12), a non-digit character
+     * and 2 digits (day, 01-31).
      * @param datetime year.month.day, e.g. '2019-10-31'.
      * @return Truncated ISO-8601 representation of the given datetime if possible, else null.
      */
@@ -151,25 +153,33 @@ public class CalendarUtils {
         Matcher ymdMatcher = YEAR_MONTH_DAY_PATTERN.matcher(datetime);
         if (ymdMatcher.matches()) {
             return ymdMatcher.group(1)
-                + "-" + ymdMatcher.group(2)
-                + "-" + ymdMatcher.group(3);
+                + DASH + ymdMatcher.group(2)
+                + DASH + ymdMatcher.group(3);
         }
         log.trace("parseYearMonthDay({}) failed parsing", datetime);
         return null;
     }
 
     private final static Pattern YEAR_TO_YEAR = Pattern.compile("^([0-9]{4}).([0-9]{4})$");
+    private final static Pattern YEAR_RANGE_PATTERN = Pattern.compile("^([0-9]{4}).(1[3-9]|[20-99]})$");
     /**
-     * Parses inputs consisting of exactly 4 digits (year), a non-digit character, 4 digits (year).
-     * @param datetime year-year, e.g. '2019-2020'.
-     * @return Truncated ISO-8601 representation of the given datetime if possible, else null.
+     * Parses inputs consisting of 4 digits (year), a non-digit character and 4 digits (year)
+     * or inputs of 4 digits (year), a non-digit character, two digits (year) in the range 13-99
+     * @param datetime year-year, e.g. '2019-2020' or 1912-13
+     * @return Truncated ISO-8601 representation of the given datetime range, else null.
      */
     private static String parseYearToYear(String datetime) {
         Matcher yyMatcher = YEAR_TO_YEAR.matcher(datetime);
         if (yyMatcher.matches()) {
             return yyMatcher.group(1)
-                + " TO " + yyMatcher.group(2);
+                + TO + yyMatcher.group(2);
         }
+        yyMatcher = YEAR_RANGE_PATTERN.matcher(datetime);
+        if (yyMatcher.matches()){
+            return yyMatcher.group(1)
+                + TO + yyMatcher.group(1).substring(0,2) + yyMatcher.group(2);
+        }
+
         log.trace("parseYearToYear({}) failed parsing", datetime);
         return null;
     }
@@ -220,7 +230,6 @@ public class CalendarUtils {
                 Date date2 = formatter2.parse(dateString);
                 return getXmlGregorianCalendar(date2).toString();
             } catch (ParseException e2) {
-//                return FALSE;
                 IllegalStateException res = new IllegalStateException("Cannot parse date '" + dateString
                     + "' in format '" + format + "'. " + "Caught exceptions: " + e + " , " + e2, e2);
                 throw res;
