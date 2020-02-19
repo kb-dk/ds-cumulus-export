8830f46a5dafd5479e2aa325302c0abf26856d2a
Toke Eskildsen <toes@kb.dk>
2020-02-19 15:55:33 +0100

    Add alternative paths to Cumulus SDK to avoid having to modify appEnv.sh when deploying

--- src/main/conf/appEnv.sh
+++ src/main/conf/appEnv.sh
@@ -9,6 +9,9 @@ APP_CONFIG=ds-cumulus-export.yaml
 JAVA_OPTS="-Xmx512m"
 
 #Optional parameter to override default classpath (lib folder)
-CLASS_PATH_OVERRIDE="/usr/local/Cumulus_Java_SDK/CumulusJC.jar:$SCRIPT_DIR/../lib/*"
+# Normally the Cumuls SDK is /usr/local/Cumulus_Java_SDK/CumulusJC.jar but the installation folder
+# can be specified during installation of the Cumul API, so we aalso sk ldconfig where it is.
+# Furthermore we att the $HOME-variant for installations where there is no root access.
+CLASS_PATH_OVERRIDE="$(ldconfig -p | grep -m 1 -o '/[^ ]*Cumulus_Java_SDK/')CumulusJC.jar:/usr/local/Cumulus_Java_SDK/CumulusJC.jar:${HOME}/Cumulus-SDK/CumulusJC.jar:$SCRIPT_DIR/../lib/*"
 
 
